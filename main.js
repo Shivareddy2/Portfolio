1e2080dda65e4b535a03d33ab42a8c843c7090b2
Shivareddy2 <144512052+Shivareddy2@users.noreply.github.com>
2025-09-10 16:38:10 +0530

    main.js

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
+// Typed.js animation for roles
 var typed = new Typed(".text", {
     strings: ["Frontend Developer", "Java Developer", "Web Developer", "Python Developer"],
-    typeSpeed: 10,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
+    typeSpeed: 100,   // typing speed
+    backSpeed: 50,    // deleting speed
+    backDelay: 1500,  // delay before deleting
+    loop: true        // loop infinitely
 });
